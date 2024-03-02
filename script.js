2c55dbdcc3efd6fb8f4181d69a94d200a165c4f3
arcadejk Kakpo <arcadej@hotmail.fr>
2024-03-02 01:32:23 +0000

    Create privacy policy html page and styling elements

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const questions = document.querySelectorAll('.question');
 
+    questions.forEach(function(question) {
+        const title = question.querySelector('.question-title');
+        const answer = question.querySelector('.answer');
+
+        title.addEventListener('click', function() {
+            answer.classList.toggle('show');
+        });
+    });
+});
   
